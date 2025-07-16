4f2eefbd9d65cc3f41ba5ba1f13cab2b9a896de9
Ubuntu <ubuntu@ip-172-31-38-226.ec2.internal>
2025-07-16 19:44:53 +0000

    Update cicdPipeline.groovy after fixing pipeline structure

--- vars/cicdPipeline.groovy
+++ vars/cicdPipeline.groovy
@@ -14,25 +14,25 @@ def call(String projectPath) {
 
     stage('BuildImage') {
         dir(projectPath) {
-            sh "docker build -t your-dockerhub-username/jenkins-app:${env.BUILD_NUMBER} ."
+            sh "docker build -t mohamedanter845/jenkins-app:${env.BUILD_NUMBER} ."
         }
     }
 
     stage('ScanImage') {
-        sh "trivy image your-dockerhub-username/jenkins-app:${env.BUILD_NUMBER} || true"
+        sh "trivy image mohamedanter845/jenkins-app:${env.BUILD_NUMBER} || true"
     }
 
     stage('PushImage') {
         withCredentials([usernamePassword(credentialsId: 'docker-hub-token', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
             sh """
                 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-                docker push your-dockerhub-username/jenkins-app:${env.BUILD_NUMBER}
+                docker push mohamedanter845/jenkins-app:${env.BUILD_NUMBER}
             """
         }
     }
 
     stage('RemoveImageLocally') {
-        sh "docker rmi your-dockerhub-username/jenkins-app:${env.BUILD_NUMBER} || true"
+        sh "docker rmi mohamedanter845/jenkins-app:${env.BUILD_NUMBER} || true"
     }
 
     stage('DeployOnK8s') {
